4948ab6a1dcf54efd21a5601a13a52193c9a544f
Pawan Prasad <pawanprasad1993@gmail.com>
2018-02-11 00:11:30 +0530

    modular chnages

--- intial_prac_code/linearRegressionProbelm.py
+++ intial_prac_code/linearRegressionProbelm.py
@@ -2,18 +2,39 @@
 """
 Created on Fri Feb  9 23:29:50 2018
 
-@author: ACER
+@author: impawan
 """
 
-import numpy as py
+import numpy as np
 import pandas as pd
 
 #setting path varaible for source file 
 path ='D:\Machine Learning\src\HeightWeightAgeGender.txt'
 
-
-data = pd.read_csv(path,sep=';',skiprows=1)
-
-print(type(data))
+#creating dataframe from source file using pandas lib
+data = pd.read_csv(path,sep=';',skiprows=1,names=['Height','Weight','Age','Male'])
 
 
+zipped = list(zip(data.Height,data.Male,data.Age,data.Weight))
+modidata = pd.DataFrame(zipped)
+
+#print (modidata)
+
+#ax = modidata.plot(kind='scatter',x=0,y=1,figsize=(12,8))
+aplha =0.001;
+iteration = 1000
+
+
+def LinearRegression(df,alpha,iteration) :
+    df.insert(0,'Ones',1)
+    col = df.shape[1];
+    x=df.iloc[:,0:col-1]
+    y=df.iloc[:,col-1:col]
+    X= np.matrix(x.values)
+    Y= np.matrix(y.values)
+    Theta = np.matrix(np.zeros(df.shape[0]))
+    print (Theta.shape)
+
+LinearRegression(modidata,aplha,iteration)
+
+    
